4053a6d34ca6fd4946688a88dd21e2fe9ac514ad
Guilherme Fontans <guilherme.fontans@gmail.com>
2021-05-31 19:38:00 -0300

    Criado crud utilizando o comando make que vem por padrão no console - bin/console make:entity - bin/console make:migration - bin/console doctrine:schema:create - bin/console make:crud

---
+++ src/Entity/Cliente.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ClienteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ClienteRepository::class)
+ */
+class Cliente
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $cpf;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getCpf(): ?int
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(int $cpf): self
+    {
+        $this->cpf = $cpf;
+
+        return $this;
+    }
+}
