170c1829a22ef47273872f42697b144696fd0417
Guilherme Fontans <guilherme.fontans@gmail.com>
2021-05-31 19:52:18 -0300

    adicionando o bootstrap e colocando o form em um container

--- src/Form/ClienteType.php
+++ src/Form/ClienteType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Cliente;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,8 @@ class ClienteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
             ->add('cpf')
         ;
     }
